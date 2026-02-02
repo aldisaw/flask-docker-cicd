6290925e7a6acd34ce7abbe952025d946dc8b465
aldisaw <aldisawah@gmail.com>
2026-02-02 22:42:47 +0700

    Initial Flask app with Dockerfiles

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ def signup():
         'phone': phone,
         'dob': dob
     }
-    return "Signup successful! You can now login.<br><a href='https://codewithbismillah.vercel.app/'>Go back</a>"
+    return "Signup successful! You can now login.<br><a href='#'>Go back</a>"
 
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['email']
     password = request.form['password']
     if email in users and users[email]['password'] == password:
-        return f"Login successful! Welcome, {users[email]['fullname']}! Happy coding <a href='https://codewithbismillah.vercel.app/'>Home</a>"
+        return f"Login successful! Welcome, {users[email]['fullname']}! Happy coding <a href='#'>Home</a>"
     return "Invalid email or password."
 
 if __name__ == '__main__':
